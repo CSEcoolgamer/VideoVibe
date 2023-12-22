579a6377f3f7100dbd021b04471670d370a7a3e5
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2023-12-22 16:27:33 +0530

    ok

---
+++ zc-one-one/src/Pages/Home/index.jsx
@@ -0,0 +1,35 @@
+import React , {useState, useCallback} from 'react';
+import { useNavigate } from 'react-router-dom';      
+
+const HomePage = () => {
+
+    const [value , setValue] = useState();
+    
+    const navigate = useNavigate()
+
+    // navigate the user to another page - \room\room_code
+    const handleJoinRoom = useCallback(() => {
+        navigate('/room/${value}')
+    }, [navigate, value]);                                  // navigate, value - these are the dependancies.
+
+    return(
+        <div>
+            <input 
+            
+            value={value} 
+            onChange={(e) => setValue(e.target.value)} 
+            type="text" 
+            placeholder='ENTER ROOM CODE'
+
+            />
+            <button onClick={handleJoinRoom}> JOIN </button>
+        </div>
+    
+    ); 
+
+};
+
+export default HomePage;
+
+
+
